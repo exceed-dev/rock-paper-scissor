bb16f503e99a2ce14d6c20c404452b2a00679971
Bagas Setia <bagas23setia@gmail.com>
2025-07-12 14:31:42 +0700

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-console.log("Hai Bagas");
-
 // create a function named getComputerChoice
 function getComputerChoice() {
   // write the code that getComputerChoice will randomy return one of the following string values: "rock", "paper", or "scissors".
   // use the Math.random method to create 3 numbers between 1 and 3 and then store them in a randomNumber variable.
-  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
 
   // console.log(randomNumber);
   // use conditional statements to make computer choice and then store the value in a computerChoice variable.
@@ -27,3 +25,14 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+// create a function named getHumanChoice
+function getHumanChoice() {
+  // write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs
+  // use the prompt method to get the user's input and store it in humanChoice, make the value case-insensitive with toLowerCase method
+  let humanChoice = prompt("choose rock, paper, or scissors").toLowerCase();
+  // use the value of humanChoice as a return
+  return humanChoice;
+}
+
+console.log(getHumanChoice());
