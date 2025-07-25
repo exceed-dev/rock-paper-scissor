e8b294c6977f5cb6a98a2b9a6cc06e19e0ed07ae
Bagas Setia <bagas23setia@gmail.com>
2025-07-25 10:58:03 +0700

    Edit playRound function

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ function getHumanChoice() {
   result = prompt("Choose Your Weapon, Rock, Paper, or Scissors");
   return result;
 }
+
+// store the players score
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  const human = humanChoice.toLowerCase();
+  const computer = computerChoice;
+  if (human === computer) {
+    console.log(`It's a tie!`);
+    console.log(`${human} ties with ${computer}`);
+  } else if (
+    (human === "rock" && computer === "scissors") ||
+    (human === "paper" && computer === "rock") ||
+    (human === "scissors" && computer === "paper")
+  ) {
+    console.log(`You won!`);
+    console.log(`${human} beats ${computer}`);
+  } else {
+    console.log(`You lose!`);
+    console.log(`${computer} beats ${human}`);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
